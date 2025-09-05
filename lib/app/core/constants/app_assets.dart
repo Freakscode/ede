80f7bb81b2af466a9426c5372c0b3358167d9bae
Ottoniel Hernandez <ottoniek14@gmail.com>
2025-09-04 20:46:41 -0500

    feat: Add AppAssets class for centralized image asset management

---
+++ lib/app/core/constants/app_assets.dart
@@ -0,0 +1,9 @@
+class AppAssets {
+  AppAssets._(); // Evita instanciación
+
+ 
+
+  // Images
+  static const String logoAlcaldia = 'assets/images/logo_alcaldia.png';
+  static const String logoDagrd = 'assets/images/logo_dagrd.png';
+}
