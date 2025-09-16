ab66bb221db6c3b6e97ddcba00d12a3660dac886
Ottoniel Hernandez <ottoniek14@gmail.com>
2025-09-16 18:39:20 -0500

    fix: Update RiskEventsScreen layout to use SingleChildScrollView for better scrolling behavior

--- lib/app/modules/risk_events/risk_events_screen.dart
+++ lib/app/modules/risk_events/risk_events_screen.dart
@@ -15,48 +15,50 @@ class RiskEventsScreen extends StatelessWidget {
         showInfo: true,
         showProfile: true,
       ),
-      body: Padding(
-        padding: EdgeInsets.zero,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const SizedBox(height: 28),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 40),
-              child: Text(
-                'Metodología de Análisis del Riesgo',
-                style: const TextStyle(
-                  color: Color(0xFF232B48),
-                  fontFamily: 'Work Sans',
-                  fontSize: 20,
-                  fontWeight: FontWeight.w600,
-                  height: 28 / 20, // line-height / font-size
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.zero,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(height: 28),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 40),
+                child: Text(
+                  'Metodología de Análisis del Riesgo',
+                  style: const TextStyle(
+                    color: Color(0xFF232B48),
+                    fontFamily: 'Work Sans',
+                    fontSize: 20,
+                    fontWeight: FontWeight.w600,
+                    height: 28 / 20, // line-height / font-size
+                  ),
                 ),
               ),
-            ),
-            const SizedBox(height: 10),
-            Center(
-              child: Text(
-                'Seleccione un evento',
-                textAlign: TextAlign.center,
-                style: const TextStyle(
-                  color: Color(0xFF706F6F),
-                  fontFamily: 'Work Sans',
-                  fontSize: 16,
-                  fontWeight: FontWeight.w600,
-                  height: 28 / 16, // line-height / font-size
+              const SizedBox(height: 10),
+              Center(
+                child: Text(
+                  'Seleccione un evento',
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(
+                    color: Color(0xFF706F6F),
+                    fontFamily: 'Work Sans',
+                    fontSize: 16,
+                    fontWeight: FontWeight.w600,
+                    height: 28 / 16, // line-height / font-size
+                  ),
                 ),
               ),
-            ),
-            const SizedBox(height: 24),
-            Expanded(
-              child: Padding(
-                padding: EdgeInsets.symmetric(horizontal: 16),
+              const SizedBox(height: 24),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
                 child: GridView.count(
                   crossAxisCount: 2,
                   mainAxisSpacing: 18,
                   crossAxisSpacing: 18,
                   childAspectRatio: 1,
+                  physics: const NeverScrollableScrollPhysics(),
+                  shrinkWrap: true,
                   children: [
                     EventCard(
                       iconAsset: AppIcons.movimientoMasa,
@@ -85,9 +87,9 @@ class RiskEventsScreen extends StatelessWidget {
                   ],
                 ),
               ),
-            ),
-            SizedBox(height: 50)
-          ],
+              SizedBox(height: 100)
+            ],
+          ),
         ),
       ),
     );
